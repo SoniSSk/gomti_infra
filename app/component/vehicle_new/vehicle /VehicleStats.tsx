e84d62edcc51ecc7654b0a1873027e4dfede58c2
SoniSSk <kuldeepsoni3099@gmail.com>
2026-09-22 21:30:01 +0530

    fix issue

--- app/component/vehicle_new/vehicle /VehicleStats.tsx
+++ app/component/vehicle_new/vehicle /VehicleStats.tsx
@@ -130,7 +130,7 @@ const STAT_CARDS: StatCard[] = [
     {
         heading: "Previous Day Vehicles",
         key: "previousPendingVehicles",
-        offset: 0,
+        offset: -4,
     },
 
     {
